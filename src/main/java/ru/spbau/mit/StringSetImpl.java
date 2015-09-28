4461e83e2cdb96fce6b9da43c8de5906e55718be
Imbd <bd.97@mail.ru>
2015-09-28 20:43:01 +0400

    hw1, v1.5

--- src/main/java/ru/spbau/mit/StringSetImpl.java
+++ src/main/java/ru/spbau/mit/StringSetImpl.java
@@ -1,213 +1,224 @@
 package ru.spbau.mit;
 
+
 import java.io.*;
 
 
 
 public class StringSetImpl implements StreamSerializable,StringSet {
 
-    private static class Node {
 
-        private int count = 0;
-        private Node[] next = new Node[charNum];
-        private boolean isTerm = false;
+	private static class Node {
 
-    }
+		private int count = 0;
+		private Node[] next = new Node[charNum];
+		private boolean isTerm = false;
 
-    private int size = 0;
-    private Node root = new Node();
-    private static final int charNum = 52;
+	}
+
+	private int size = 0;
+	private Node root = new Node();
+	private static final int charNum = 52;
 
 
-    private int number(char c){
-        if (Character.isUpperCase(c)) {
+	private int number(char c) {
+		if (Character.isUpperCase(c)) {
 			return (c - 'A');
-        }
-        else {
-            return (c - 'a' + 26);
-        }
-    }
+		}
+		else {
+			return (c - 'a' + 26);
+		}
+	}
 
-    public boolean add(String element) {
 
-        Node curNode = root;
-        if (contains(element)) {
+	public boolean add(String element) {
+
+		Node curNode = root;
+		if (contains(element)) {
 			return false;
 		}
-        root.count++;
-
-		for (char c: element.toCharArray()) {
-            int num = number(c);
+		root.count++;
+		for (int i = 0; i < element.length(); i++) {
+			int num = number(element.charAt(i));
 			if (curNode.next[num] != null) {
-                curNode.next[num].count++;
-            }
-            else {
-                curNode.next[num] = new Node();
-                curNode.next[num].count++;
-            }
+				curNode.next[num].count++;
+			}
+			else {
+				curNode.next[num] = new Node();
+				curNode.next[num].count++;
+			}
 
-            curNode = curNode.next[num];
-        }
+			curNode = curNode.next[num];
+		}
 
-        curNode.isTerm = true;
-        size++;
-        return true;
+		curNode.isTerm = true;
+		size++;
+		return true;
 
-    }
+	}
 
-    public boolean contains(String element) {
-        Node curNode = root;
-		for (char c: element.toCharArray()) {
-			int num = number(c);
-            if (curNode.next[num] == null) {
+
+	public boolean contains(String element) {
+		Node curNode = root;
+		for (int i = 0; i < element.length(); i++) {
+			int num = number(element.charAt(i));
+			if (curNode.next[num] == null) {
 				return false;
 			}
-            curNode = curNode.next[num];
-        }
-        return curNode.isTerm;
-    }
+			curNode = curNode.next[num];
+		}
+		return curNode.isTerm;
+	}
 
-    public boolean remove(String element) {
 
-        Node curNode = root;
-        if (!contains(element)) {
+	public boolean remove(String element) {
+
+		Node curNode = root;
+		if (!contains(element)) {
 			return false;
 		}
-        root.count--;
-		for (char c: element.toCharArray()) {
-			int num = number(c);
-            curNode.next[num].count--;
-            if (curNode.next[num].count == 0) {
-                curNode.next[num] = null;
-                return true;
-            }
-            else {
+		root.count--;
+		for (int i = 0; i < element.length(); i++) {
+
+			int num = number(element.charAt(i));
+			curNode.next[num].count--;
+			if (curNode.next[num].count == 0) {
+				Node tmpNode = curNode;
 				curNode = curNode.next[num];
 			}
-        }
-
-        size--;
-        curNode.isTerm = false;
-        return true;
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public int howManyStartsWithPrefix(String prefix) {
-
-		if (!contains(prefix)) {
-			return 0;
+			else {
+				curNode = curNode.next[num];
+			}
 		}
+
+		size--;
+		curNode.isTerm = false;
+		return true;
+	}
+
+
+	public int size() {
+		return size;
+	}
+
+	public int howManyStartsWithPrefix(String prefix) {
+
 		Node curNode = root;
-		for (char c: prefix.toCharArray()) {
-			int num = number(c);
-            curNode = curNode.next[num];
-        }
-        return curNode.count;
-    }
+		for (int i = 0; i < prefix.length(); i++) {
+			int num = number(prefix.charAt(i));
+			if (curNode.next[num] == null) {
+				return 0;
+			}
+			curNode = curNode.next[num];
+		}
+		return curNode.count;
+	}
 
-    private void makeOutput(Node curNode, OutputStream out) throws IOException {
 
-        byte[] b = new byte[4];
-        int a;
-        for (int i = 0; i < charNum; i++) {
+	private void makeOutput(Node curNode, OutputStream out) throws IOException {
 
-            if (curNode.next[i] == null || curNode.next[i].count == 0) {
+		byte[] b = new byte[4];
+		int a;
+		for (int i = 0; i < charNum; i++) {
 
-                a = -1;
-                b[3] = (byte) a;
-                b[2] = (byte) ((a >> 8));
-                b[1] = (byte) ((a >> 16));
-                b[0] = (byte) ((a >> 24));
-                out.write(b);
-            } else {
+			if (curNode.next[i] == null || curNode.next[i].count == 0) {
 
-                a = curNode.next[i].count;
-                b[3] = (byte) a;
-                b[2] = (byte) ((a >> 8));
-                b[1] = (byte) ((a >> 16));
-                b[0] = (byte) ((a >> 24));
-                out.write(b);
-                byte[] x = new byte[1];
-                x[0] = (byte) (curNode.next[i].isTerm ? 1 : 0);
-                out.write(x);
-                makeOutput(curNode.next[i], out);
+				a = -1;
+				b[3] = (byte) a;
+				b[2] = (byte) ((a >> 8));
+				b[1] = (byte) ((a >> 16));
+				b[0] = (byte) ((a >> 24));
+				out.write(b);
+			}
+			else {
 
-            }
+				a = curNode.next[i].count;
+				b[3] = (byte) a;
+				b[2] = (byte) ((a >> 8));
+				b[1] = (byte) ((a >> 16));
+				b[0] = (byte) ((a >> 24));
+				out.write(b);
+				byte[] x = new byte[1];
+				x[0] = (byte) (curNode.next[i].isTerm ? 1 : 0);
+				out.write(x);
+				makeOutput(curNode.next[i], out);
 
-        }
-    }
+			}
 
-    public void serialize(OutputStream out) {
+		}
+	}
 
-        try {
-            byte[] b = new byte[4];
-            int a = root.count;
-            b[3] = (byte) a;
-            b[2] = (byte) ((a >> 8));
-            b[1] = (byte) ((a >> 16));
-            b[0] = (byte) ((a >> 24));
-            out.write(b);
+	public void serialize(OutputStream out) {
 
-            byte[] x = new byte[1];
-            x[0] = (byte) (root.isTerm ? 1 : 0);
-            out.write(x);
+		try {
+			byte[] b = new byte[4];
+			int a = root.count;
+			b[3] = (byte) a;
+			b[2] = (byte) ((a >> 8));
+			b[1] = (byte) ((a >> 16));
+			b[0] = (byte) ((a >> 24));
+			out.write(b);
 
-            makeOutput(root, out);
-        } 
-        catch (IOException io) {
-            throw new SerializationException();
-        }
-    }
+			byte[] x = new byte[1];
+			x[0] = (byte) (root.isTerm ? 1 : 0);
+			out.write(x);
 
-    private void makeTree(Node curNode, InputStream in) throws IOException{
+			makeOutput(root, out);
+		}
+		catch (IOException io) {
+			throw new SerializationException();
+		}
+	}
 
-        byte[] data = new byte[4];
-        byte[] x = new byte[1];
-        int num;
-        for (int i = 0; i < charNum; i++) {
 
-            int k = in.read(data);
-            num = (data[0] << 24) | (data[1] & 0xFF) << 16 | (data[2] & 0xFF) << 8 | (data[3] & 0xFF);
+	private void makeTree(Node curNode, InputStream in) throws IOException {
 
-            if (num != -1) {
+		byte[] data = new byte[4];
+		byte[] x = new byte[1];
+		int num;
+		for (int i = 0; i < charNum; i++) {
 
-                int l = in.read(x);
-                curNode.next[i] = new Node();
-                curNode.next[i].isTerm = (x[0] == 1);
-                curNode.next[i].count = num;
-                makeTree(curNode.next[i], in);
-            }
+			int k = in.read(data);
+			num = (data[0] << 24) | (data[1] & 0xFF) << 16 | (data[2] & 0xFF) << 8 | (data[3] & 0xFF);
 
-        }
+			if (num != -1) {
 
-    }
+				int l = in.read(x);
+				curNode.next[i] = new Node();
+				curNode.next[i].isTerm = (x[0] == 1);
+				curNode.next[i].count = num;
+				makeTree(curNode.next[i], in);
+			}
 
-    public void deserialize(InputStream in) {
+		}
 
-        root = new Node();
-        size = 0;
+	}
 
-        try {
+	public void deserialize(InputStream in) {
 
-            byte[] data = new byte[4];
-            byte[] x = new byte[1];
-            int k = in.read(data);
-            int num = (data[0] << 24) | (data[1] & 0xFF) << 16 | (data[2] & 0xFF) << 8 | (data[3] & 0xFF);
+		root = new Node();
+		size = 0;
 
-            if (num != -1) {
-                int l = in.read(x);
-                root.isTerm = (x[0] == 1);
-                root.count = num;
-            }
 
-            makeTree(root, in);
-        }
-        catch (IOException io) {
-            throw new SerializationException();
-        }
-        size = root.count;
-    }
+		try {
+
+			byte[] data = new byte[4];
+			byte[] x = new byte[1];
+			int k = in.read(data);
+			int num = (data[0] << 24) | (data[1] & 0xFF) << 16 | (data[2] & 0xFF) << 8 | (data[3] & 0xFF);
+
+			if (num != -1) {
+				int l = in.read(x);
+				root.isTerm = (x[0] == 1);
+				root.count = num;
+			}
+
+			makeTree(root, in);
+		}
+		catch (IOException io) {
+			throw new SerializationException();
+		}
+
+		size = root.count;
+	}
 }
