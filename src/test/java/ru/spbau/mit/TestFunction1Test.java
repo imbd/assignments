873fe3baf82719fa18d0ea767df39bb6125a9ff8
Imbd <bd.97@mail.ru>
2015-10-04 23:24:56 +0400

    HW2 v1.0

---
+++ src/test/java/ru/spbau/mit/TestFunction1Test.java
@@ -0,0 +1,13 @@
+package ru.spbau.mit;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by YARO on 04.10.2015.
+ */
+public class TestFunction1Test extends TestCase {
+
+    public void testTestCompose() throws Exception {
+
+    }
+}
